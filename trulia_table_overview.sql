7ce5b514139122d0957cdf5222f95a2abd597edd
root <root@localhost.localdomain>
2022-07-29 21:11:27 -0400

    added tables

--- trulia_table_overview.sql
+++ trulia_table_overview.sql
@@ -12,3 +12,20 @@ CREATE TABLE "public"."TruliaOverview" (
     "zipCode" character varying(50) NOT NULL,
     CONSTRAINT "TruliaOverview_street_city_state_retrievedTimeStamp" PRIMARY KEY ("street", "city", "state", "retrievedTimeStamp")
 ) WITH (oids = false);
+
+
+
+
+
+DROP TABLE IF EXISTS "TruliaPropertyDetails";
+CREATE TABLE "public"."TruliaPropertyDetails" (
+    "street" character varying(100) NOT NULL,
+    "city" character varying(100) NOT NULL,
+    "state" character varying(50) NOT NULL,
+    "retrievedTimeStamp" character varying(100) NOT NULL,
+    "link" text NOT NULL,
+    "description" text NOT NULL,
+    "yearBuilt" character varying(50) NOT NULL,
+    "daysOnMarket" character varying(25) NOT NULL,
+    "currentMarketStatus" character varying(50) NOT NULL
+) WITH (oids = false);
